ac74fa17d1b867bf3de902bc22f0b6f5f5d7ef72
TineoC <christophertineo02@gmail.com>
2022-01-05 14:05:30 -0400

    initialize project

---
+++ source.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+    Fecha de Inicio: 4 - 11 - 2021
+
+    Fecha de Entrega: 6 - 11 - 2021
+
+    Grupo #02
+
+    * Christopher Tineo
+    * Camilo Maria
+    * Lya Sanchez
+    * Sahira Tejada
+    * Taiyab Gillani
+    * José Fernandez
+
+    * Instrucciones
+
+    Realizar un programa C++ que dado la ubicación de 2 torres enemigas y una Reina, se produzca una salida con todas las jugadas posibles de la reina donde puede y no ser eliminada.
+*/
+
+int main() {
+    // Validad entrada
+
+    // Imprimir mesa de ajedrez
+}
